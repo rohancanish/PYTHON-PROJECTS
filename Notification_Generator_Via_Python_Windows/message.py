2dea1a0988da07e3941fcd1677484f9d1759f3ec
rohancanish <114724040+rohancanish@users.noreply.github.com>
2024-08-11 12:53:53 +0530

    Create message.py

---
+++ Notification_Generator_Via_Python_Windows/message.py
@@ -0,0 +1,13 @@
+#FIRST , Install plyer
+# pip install plyer 
+# Used to install plyer which is needed for the program
+
+
+from plyer import notification
+
+
+notification.notify(
+    title="Hi",
+    message="hi , take a break",
+    app_name="python coding"
+)
