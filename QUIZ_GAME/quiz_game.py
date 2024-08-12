8f630dbd8d557e416b24b0df356ad437fe59ddbb
rohancanish <114724040+rohancanish@users.noreply.github.com>
2024-08-12 11:57:54 +0530

    Update quiz_game.py

--- QUIZ_GAME/quiz_game.py
+++ QUIZ_GAME/quiz_game.py
@@ -3,10 +3,11 @@ print("1.Animals,2.Olympics")
 a=input(print("Please CHoose Topic of Interest"))
 
 
-if(a==1):
+if a=='1':
   print("1st question!!")
-  answer=intput(print("Which animal is known as the king of the jungle"))
-    if answer.lower()=="lion"
+  answer=input("Which animal is known as the king of the jungle").lower()
+    
+if answer=="lion":
               print("correct")
 
 print("Game is completed!!!")
